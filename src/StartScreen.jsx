e13ef25a8ccaf35e46acdb7252de9cff010916fc
solabest <soltad65@gmail.com>
2024-04-21 03:43:05 -0700

    update title and fav icon

--- src/StartScreen.jsx
+++ src/StartScreen.jsx
@@ -1,17 +1,17 @@
-
-// eslint-disable-next-line react/prop-types
-function StartScreen({ numQuestions ,dispatch}) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div>
-      <h2>Welcome to The react Quiz~</h2>
-      <p>{numQuestions} question to test your React mastery</p>
+      <h2>Welcome to The react Quiz</h2>
+      <h3>{numQuestions} question to test your React mastery</h3>
 
-      <button className="btn btn-ui" onClick={()=>dispatch({type:'start'})} >
-    
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
         Let's start
       </button>
     </div>
   );
 }
 
-export default StartScreen
+export default StartScreen;
